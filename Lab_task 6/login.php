86b7df10aabe6e969ffb5e1b96d78686559a164a
Arman Hossain <60995050+AArmanH20@users.noreply.github.com>
2021-03-23 22:41:31 +0600

    php session and cookie

---
+++ Lab_task 6/login.php
@@ -0,0 +1,19 @@
+<style type="text/css">
+	th{
+		text-align: right;
+	}
+	h3{
+		text-align: center;
+	}
+</style>
+<table cellpadding="5" cellpadding="10" align="center">
+	<h3> Login form using session and cookies with remember me</h3>
+	<form method="post" action="validate.php">
+		<tr><th>Email</th><td><input type="test" name="email"></td></tr>
+		<tr><th>Password</th><td><input type="password" name="password"></td></tr>
+		<tr><td colspan="2" align="center"> <input type="checkbox" name="remember" value="1">Remember Me</td></tr>
+		<tr><td colspan="2" align="right"> <input type="submit" name="login" value="Login">Remember Me</td></tr>
+		
+	</form>
+	
+</table>
